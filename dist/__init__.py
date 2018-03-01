a560f4de1359f10feb4417d93d4e5ebd6c618af3
phasewalker18 <36952218+phasewalker18@users.noreply.github.com>
2018-03-01 09:35:15 +0000

    Create __init__.py

---
+++ dist/__init__.py
@@ -0,0 +1,5 @@
+# Included in the dist folder are any pre-requisites
+# That aren't available via the normal distribution
+# Channels.
+# All rights to these files remain reserved by
+# the original authors.
